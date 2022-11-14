b3caeb241bf7e7c09c2d2e3e2dddc0a3b41dcc3c
IkhsanMardika <ikhsancahyamardika@gmail.com>
2022-11-14 14:40:48 +0700

    Initial commit

---
+++ app/src/main/java/com/c22027/nyoombang/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c22027.nyoombang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
