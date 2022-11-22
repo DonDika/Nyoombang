3a1ed86607b5a1b265dee54e7485fd121f297be2
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-11-22 23:26:58 +0700

    Membuat Util Format Date dan Time

--- app/src/main/java/com/c22027/nyoombang/utils/Utilization.kt
+++ app/src/main/java/com/c22027/nyoombang/utils/Utilization.kt
@@ -1,11 +1,23 @@
 package com.c22027.nyoombang.utils
 
-/*
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
 object Utilization {
 
     //Implement here
-    to put common function or variable
+    fun formatDate(currentDateString: String, targetTimeZone: String): String {
+        val instant = Instant.parse(currentDateString)
+        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+            .withZone(ZoneId.of(targetTimeZone))
+        return formatter.format(instant)
+    }
 
+    fun formatTime(currentDateString: String, targetTimeZone: String): String {
+        val instant = Instant.parse(currentDateString)
+        val formatter = DateTimeFormatter.ofPattern("HH:mm")
+            .withZone(ZoneId.of(targetTimeZone))
+        return formatter.format(instant)
+    }
 }
-
-*/
