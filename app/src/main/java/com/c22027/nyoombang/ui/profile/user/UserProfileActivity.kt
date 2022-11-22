00f515232c606add24548b9b2b16c7119efc7109
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-11-22 23:26:15 +0700

    Feature Profile User : Membuat UI Profile User

--- app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileActivity.kt
+++ app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileActivity.kt
@@ -1,2 +1,45 @@
 package com.c22027.nyoombang.ui.profile.user
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.c22027.nyoombang.databinding.ActivityUserProfileBinding
+import com.c22027.nyoombang.ui.adapter.DonationListAdapter
+
+class UserProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserProfileBinding
+    private val viewModel: UserProfileViewModel by viewModels()
+    private val adapter = DonationListAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        val data = viewModel.getUser()
+        binding.apply {
+            Glide.with(this@UserProfileActivity)
+                .load(data.image)
+                .into(civProfile)
+
+            edtName.setText(data.name)
+            edtEmail.setText(data.email)
+        }
+
+        val donations = viewModel.getDonations()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvDonation.layoutManager = layoutManager
+        binding.rvDonation.setHasFixedSize(true)
+        binding.rvDonation.adapter = adapter
+
+        adapter.submitList(donations)
+    }
+}
--- app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileViewModel.kt
+++ app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileViewModel.kt
@@ -1,6 +1,11 @@
 package com.c22027.nyoombang.ui.profile.user
 
-class UserProfileViewModel {
+import androidx.lifecycle.ViewModel
+import com.c22027.nyoombang.data.local.Donation
+import com.c22027.nyoombang.data.local.User
 
+class UserProfileViewModel : ViewModel() {
+    fun getUser() = User.normalUser
 
+    fun getDonations() = Donation.provideDonations()
 }
