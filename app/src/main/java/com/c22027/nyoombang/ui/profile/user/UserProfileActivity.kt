237d6d73b0df1441b6f5a137c942ce5c52bc2515
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-12-02 20:51:51 +0700

    Feature Profile: User Profile

--- app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileActivity.kt
+++ app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileActivity.kt
@@ -3,15 +3,20 @@ package com.c22027.nyoombang.ui.profile.user
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.content.ContextCompat
+//import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.c22027.nyoombang.R
 import com.c22027.nyoombang.databinding.ActivityUserProfileBinding
-import com.c22027.nyoombang.ui.adapter.DonationListAdapter
+import com.c22027.nyoombang.helper.SharedPreferencesHelper
+
+//import com.c22027.nyoombang.ui.adapter.DonationListAdapter
 
 class UserProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUserProfileBinding
+    private lateinit var preferences: SharedPreferencesHelper
     private val viewModel: UserProfileViewModel by viewModels()
-    private val adapter = DonationListAdapter()
+//    private val adapter = DonationListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,27 +24,31 @@ class UserProfileActivity : AppCompatActivity() {
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        preferences = SharedPreferencesHelper(this)
+
         setupView()
     }
 
     private fun setupView() {
-        val data = viewModel.getUser()
-        binding.apply {
-            Glide.with(this@UserProfileActivity)
-                .load(data.image)
-                .into(civProfile)
+        viewModel.getUser(preferences.prefUid.toString()).observe(this) {
+            binding.apply {
+                Glide.with(this@UserProfileActivity)
+                    .load(it.items!![0].picture)
+                    .error(ContextCompat.getDrawable(this@UserProfileActivity, R.drawable.icon_account))
+                    .into(civProfile)
 
-            edtName.setText(data.name)
-            edtEmail.setText(data.email)
+                edtName.setText(it.items!![0].name)
+                edtEmail.setText(it.items!![0].email)
+            }
         }
 
-        val donations = viewModel.getDonations()
-
-        val layoutManager = LinearLayoutManager(this)
-        binding.rvDonation.layoutManager = layoutManager
-        binding.rvDonation.setHasFixedSize(true)
-        binding.rvDonation.adapter = adapter
-
-        adapter.submitList(donations)
+//        val donations = viewModel.getDonations()
+//
+//        val layoutManager = LinearLayoutManager(this)
+//        binding.rvDonation.layoutManager = layoutManager
+//        binding.rvDonation.setHasFixedSize(true)
+//        binding.rvDonation.adapter = adapter
+//
+//        adapter.submitList(donations)
     }
 }
--- app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileViewModel.kt
+++ app/src/main/java/com/c22027/nyoombang/ui/profile/user/UserProfileViewModel.kt
@@ -1,11 +1,12 @@
 package com.c22027.nyoombang.ui.profile.user
 
 import androidx.lifecycle.ViewModel
-import com.c22027.nyoombang.data.local.Donation
-import com.c22027.nyoombang.data.local.User
+//import com.c22027.nyoombang.data.local.Donation
+//import com.c22027.nyoombang.data.local.User
+import com.c22027.nyoombang.data.remote.repository.AppsRepositoryImpl
 
-class UserProfileViewModel : ViewModel() {
-    fun getUser() = User.normalUser
+class UserProfileViewModel(private val repository: AppsRepositoryImpl = AppsRepositoryImpl()) : ViewModel() {
+    fun getUser(id: String) = repository.getUser(id)
 
-    fun getDonations() = Donation.provideDonations()
+//    fun getDonations() = Donation.provideDonations()
 }
