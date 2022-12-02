b7b771f3269886e68692f14a5dcfea70ae667db7
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-12-02 20:49:13 +0700

    Feature Util Date

--- app/src/main/java/com/c22027/nyoombang/utils/Utilization.kt
+++ app/src/main/java/com/c22027/nyoombang/utils/Utilization.kt
@@ -7,17 +7,17 @@ import java.time.format.DateTimeFormatter
 object Utilization {
 
     //Implement here
-    fun formatDate(currentDateString: String, targetTimeZone: String): String {
-        val instant = Instant.parse(currentDateString)
-        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
-            .withZone(ZoneId.of(targetTimeZone))
-        return formatter.format(instant)
-    }
-
-    fun formatTime(currentDateString: String, targetTimeZone: String): String {
-        val instant = Instant.parse(currentDateString)
-        val formatter = DateTimeFormatter.ofPattern("HH:mm")
-            .withZone(ZoneId.of(targetTimeZone))
-        return formatter.format(instant)
-    }
+//    fun formatDate(currentDateString: String, targetTimeZone: String): String {
+//        val instant = Instant.parse(currentDateString)
+//        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+//            .withZone(ZoneId.of(targetTimeZone))
+//        return formatter.format(instant)
+//    }
+//
+//    fun formatTime(currentDateString: String, targetTimeZone: String): String {
+//        val instant = Instant.parse(currentDateString)
+//        val formatter = DateTimeFormatter.ofPattern("HH:mm")
+//            .withZone(ZoneId.of(targetTimeZone))
+//        return formatter.format(instant)
+//    }
 }
